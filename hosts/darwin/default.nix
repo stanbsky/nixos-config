3aed4cc8eb7975093c1fbe2b2d5339ae38f44a2d
Stan Borzhemsky <stanbsky@protonmail.com>
2026-04-26 20:31:30 +0100

    Add zed cachix

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -18,12 +18,14 @@ let user = "stanborzhemsky"; in
         "https://nix-community.cachix.org"
         "https://devenv.cachix.org"
         "https://sadjow.cachix.org"
+        "https://zed-industries.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "sadjow.cachix.org-1:hCRSO0bM0seFNBZsUVKSzTzgCCFoU7vsCInDmkzHgEg="
+        "zed-industries.cachix.org-1:fgVpvtdF+ssrgP1lB6EusuR3uM6bNcncWduKxri3u6Y="
       ];
     };
 
